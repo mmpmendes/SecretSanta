f070a720c8d94236e8c35ccad89c384a7cdb9ee1
Márcio Mendes <mmpmendes@users.noreply.github.com>
2024-11-04 02:49:14 -0100

    added page to manage friend number and roll / calculate draws on the client side To be moved to the server side

---
+++ SecretSanta.Web/Models/Amigo.cs
@@ -0,0 +1,17 @@
+﻿namespace SecretSanta.Web.Models;
+
+public class Amigo
+{
+    public Amigo() { }
+
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public Amigo? AmigoSorteado { get; set; }
+
+    public Amigo(Amigo otherAmigo)
+    {
+        Nome = otherAmigo.Nome;
+        Email = otherAmigo.Email;
+        AmigoSorteado = otherAmigo.AmigoSorteado;
+    }
+}
---
+++ SecretSanta.Web/Models/Par.cs
@@ -0,0 +1,7 @@
+﻿namespace SecretSanta.Web.Models;
+
+public class Par<T>
+{
+    public T Dador { get; set; }
+    public T Recebedor { get; set; }
+}
