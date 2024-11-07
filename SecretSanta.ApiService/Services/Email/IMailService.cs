44704ca17e6234756b2afde41e21aede8a286547
Márcio Mendes <mmpmendes@users.noreply.github.com>
2024-11-06 23:13:15 -0100

    Removed sensitive information

---
+++ SecretSanta.ApiService/Services/Email/IMailService.cs
@@ -0,0 +1,7 @@
+﻿
+namespace SecretSanta.ApiService.Services.Email;
+
+public interface IMailService
+{
+    bool SendMail(string recipientEmail, string giverName, string receiverEmail, string receiverName);
+}
